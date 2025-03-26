5bb0936d8f99f089edbfd53a036d687935bba857
AmineFouzai <thehanterx2@gmail.com>
2025-03-26 01:18:50 +0100

    New Updates

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,55 +2,58 @@ import * as path from 'node:path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
+  markdown: {
+    checkDeadLinks: true,
+  },
   root: path.join(__dirname, 'app'),
   title: 'Mohamed Amine Fouzai | Portfolio',
   icon: '/me.png',
   base:"/",
   logo: {
-    light: '/me.png',
-    dark: '/me.png',
+    light: '/light.png',
+    dark: '/dark.png',
   },
   ssg: {
     strict: true
   },
   themeConfig: {
-  
-    sourceCodeText:"text",
+    outlineTitle:"Within This Page",
     footer: {
       message:
         'Made with ❤️ by Fouzai Mohamed Amine',
     },
+
     nav: [
     
       {
-        "text": "Education & Cerfications",
+        "text": "👨🏼‍🎓 Education & Cerfications",
         "link": "/pages/education",
         "activeMatch": "/pages/education",
         "position":"right"
       },
       {
-        "text": "Work Experience",
+        "text": "👨🏼‍💻 Work Experience",
         "link": "/pages/experience/",
         "activeMatch": "/pages/experience/",
          "position":"right"
       },
       {
-        "text": "Portfolio Pieces",
+        "text": "💼 Portfolio Pieces",
         "link": "/pages/projects/",
         "activeMatch": "/pages/projects/",
          "position":"right"
       },
     
       {
-        "text": "Contact & Resume",
+        "text": "📞 Contact & Resume",
         "link": "/pages/contact",
         "activeMatch": "/pages/contact",
          "position":"right"
       },
       {
-        "text": "Blog",
-        "link": "/pages/contact",
-        "activeMatch": "/pages/contact",
+        "text": "ℹ️ Blog",
+        "link": "/pages/blog",
+        "activeMatch": "/pages/blog",
          "position":"right"
       }
       
